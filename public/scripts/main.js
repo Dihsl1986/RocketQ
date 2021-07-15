c00d97027c7b321695c9adcd3fa6fd13c6b41f93
Diego Santos Lima <dihsl1986@gmail.com>
2021-07-15 05:45:35 -0300

    Atualizando aula terminando a modal usando eventListener e editando modal.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,29 +3,39 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescrition = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 //Pegar todos os botões que existem com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
-checkButtons.forEach( button => {
+checkButtons.forEach(button => {
     //Adicionar a escuta
-    button.addEventListener("click", event => {
-        //Abrir modal
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
+    //Abrir a modal
 })
-
 // Pegar quando o marcar como lido for clicado
-
-
 //Abrir modal
-
-modal.open( )
 
 //Quando o botão delete/excluir for clicado ele ira abrir a modal 
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", event =>{
-        modal.open()
+    button.addEventListener("click", (event) => handleClick(event, false))
+        
     })
-})
+
+
+function handleClick(event, check = true){
+    event.preventDefault()
+    const text = check ? "Marcar como lida" : "Excluir"
+
+    modalTitle.innerHTML= `${text} esta pergunta`
+    modalDescrition.innerHTML= `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`
+    modalButton.innerHTML=`Sim, ${text.toLocaleLowerCase()}`
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+     
+    //Abrir modal
+    modal.open()
+}
--- src/route.js
+++ src/route.js
@@ -6,5 +6,9 @@ route.get('/', (req, res) => res.render("index"))
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
 
+//Formato que dentro da modal tem que passar a informação
+// route.post('/room/:room/:question/:action')
+
+
 module.exports = route 
 
